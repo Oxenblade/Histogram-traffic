b87c6970de67712c805ae90fb00e7517f411579d
Oxenblade <hemiwysr07rox@gmail.com>
2025-03-24 12:37:56 +0530

    start of the tkinter project

---
+++ histogram tkinter.py
@@ -0,0 +1,32 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+root= Tk()
+root.title("Traffic data on Histogram")
+root.iconbitmap("C:/Users/hp/Downloads/traffic data/statistic_9684905.ico")
+
+frame = LabelFrame(root, text= "Histogram....", padx=40, pady=40, relief =SUNKEN)
+frame.pack(padx=10, pady = 10)
+
+
+my_img_1 = ImageTk.PhotoImage(Image.open("images/histogram.PNG"))
+
+my_label = Label(frame, image= my_img_1)
+my_label.grid(row=3, column=0, columnspan =3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
